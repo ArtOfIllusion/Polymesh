23e51787ec1da8d8ebbb1db68ae184143c1b6aae
makiam <maxtemplaar@gmail.com>
2024-01-25 23:22:52 +0300

    Update PolyMeshViewer.java
    Fix color select condition

--- src/artofillusion/polymesh/PolyMeshViewer.java
+++ src/artofillusion/polymesh/PolyMeshViewer.java
@@ -357,7 +357,7 @@ public class PolyMeshViewer extends MeshViewer
             else
                 ref = i;
 
-            Color color = selected[ref] && visible[i] ? selectedVertColor : vertColor;
+            Color color = !selected[ref] && visible[i] ? selectedVertColor : vertColor;
 
             if (renderMode == RENDER_WIREFRAME || renderMode == RENDER_TRANSPARENT)
             {
